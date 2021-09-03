a46635b555eb9ba70898f074f50f5253dd0b0028
Bkaraba <bkaraba14@gmail.com>
2021-09-03 04:38:22 -0700

    patch

--- parsing.c
+++ parsing.c
@@ -6,24 +6,13 @@
  */
 int count(char *buffer)
 {
-	int pearls = 0;
-	char *delimiter = " =:'\n''\t'";
 	int i = 0;
-	int j = 0;
-
+	
 	while (buffer[i] != '\0')
 	{
-		for (j = 0; delimiter[j] != '\0'; j++)
-		{
-			if (buffer[i] == delimiter[j])
-			{
-				pearls++;
-				break;
-			}
-		}
 		i++;
 	}
-	return (pearls + 1);
+	return (i++);
 }
 
 /**
